9be24e66610aea14dd398e925bf296855e1f3cf3
winsonsun1206 <winsonsun1206@gmail.com>
2026-03-25 15:28:43 +0800

    submit changes on requirements

--- ActuatorTest/ActuatorTestDemo/utils/station_conf.py
+++ ActuatorTest/ActuatorTestDemo/utils/station_conf.py
@@ -5,7 +5,7 @@ import os
 samples of station_conf.txt:
 station_name: station_1
 """
-station_config = "~/station_conf.txt"
+station_config = "$HOME/station_conf.txt"
 
 def read_station_conf():
     with open(os.path.expanduser(station_config), "r") as f:
