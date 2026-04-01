6be7fd945c0c195be22b32f32e1ca24892356790
winsonsun1206 <winsonsun1206@gmail.com>
2026-04-01 16:42:43 +0800

    station name

--- ActuatorTest/ActuatorTestDemo/utils/station_conf.py
+++ ActuatorTest/ActuatorTestDemo/utils/station_conf.py
@@ -1,17 +1,17 @@
 
 import os
-
+from pathlib import Path
 """
 samples of station_conf.txt:
 station_name: station_1
 """
-station_config = "$HOME/station_conf.txt"
+station_config = str(Path.home() / "station_conf.txt")
 
 def read_station_conf():
-    with open(os.path.expanduser(station_config), "r") as f:
+    with open(station_config, "r") as f:
         lines = f.readlines()
         conf = {}
         for line in lines:
             key, value = line.strip().split(":")
             conf[key] = value
-    return conf
+    return conf.strip()
