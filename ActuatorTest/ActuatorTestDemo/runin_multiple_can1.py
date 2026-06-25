4752da62545ab785729b126f53b5b0c90eda1b6c
winsonsun1206 <winsonsun1206@gmail.com>
2026-06-25 15:23:27 +0800

    add termination

--- ActuatorTest/ActuatorTestDemo/runin_multiple_can1.py
+++ ActuatorTest/ActuatorTestDemo/runin_multiple_can1.py
@@ -335,17 +335,20 @@ class RabbitmqCusumer:
                         self.redis_handler.set_value(task_id, 0.0)
                         calibrate_motor_parameter(part_numbers, serial_numbers, can_msg_addresses) 
                         #time.sleep(150)
-                        wait_for_termination(self.redis_handler, task_id, check_interval=0.3, total_timeout=150)                       
+                        if wait_for_termination(self.redis_handler, task_id, check_interval=0.3, total_timeout=150) == "termination detected":
+                            break                       
                         self.redis_handler.set_value(task_id, 0.33)                       
                         calibrate_encoder_parameter(part_numbers, serial_numbers, can_msg_addresses)
                         #heartbeat_calibration(can_msg_addresses, timeout=230)
                         #time.sleep(45)
-                        wait_for_termination(self.redis_handler, task_id, check_interval=0.3, total_timeout=45) 
+                        if wait_for_termination(self.redis_handler, task_id, check_interval=0.3, total_timeout=45) == "termination detected":
+                            break
                         self.redis_handler.set_value(task_id, 0.67)                        
                         calibrate_electrical_parameter(part_numbers, serial_numbers, can_msg_addresses)
                         #heartbeat_calibration(can_msg_addresses, timeout=230)
                         #time.sleep(30)
-                        wait_for_termination(self.redis_handler, task_id, check_interval=0.3, total_timeout=30)
+                        if wait_for_termination(self.redis_handler, task_id, check_interval=0.3, total_timeout=30) == "termination detected":
+                            break
                         save_parameters_to_flash(part_numbers, serial_numbers, can_msg_addresses)
                         client_socket.sendto(json.dumps({"message": "task finished"}).encode('utf-8'), (HOST, UDP_PORT))
                         print("Calibration process completed.")
